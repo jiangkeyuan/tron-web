c608a36ce7e61e3cf8d90177057b3526f220208c
HelloWorld <1079837102@qq.com>
2023-03-06 23:48:31 +0800

    feat:add home banner

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,9 +1,10 @@
 <template>
   <Header></Header>
+  <router-view></router-view>
 </template>
 
 <script setup>
- import Header from "@/components/Header.vue";
+import Header from '@/components/Header.vue'
 import { ref } from 'vue'
 const handleSelect = (key, keyPath) => {
   console.log(key, keyPath)
--- src/router/index.js
+++ src/router/index.js
@@ -4,74 +4,87 @@ const routes = [
   {
     path: '/',
     component: () => import('../page/home/index.vue'),
+    children: [
+      {
+        path: '/',
+        components: {
+          helper: () => import('../page/home/index/index.vue'),
+          default: () => import('../page/home/index/index.vue')
+        }
+      },
+      {
+        path: 'c2c',
+        components: {
+          helper: () => import('../page/home/c2c/index.vue'),
+          default: () => import('../page/home/c2c/index.vue')
+        }
+      }
+    ]
   },
   {
     path: '/auth/login',
-    component: () => import('../page/login/index.vue'),
+    component: () => import('../page/login/index.vue')
   },
   {
     path: '/buyer',
     component: () => import('../page/buyer/index.vue'),
-    children:[
+    children: [
       {
         path: 'dashboard',
-        components:{
+        components: {
           helper: () => import('../page/buyer/dashbord/index.vue'),
-          default:() => import('../page/buyer/dashbord/index.vue'),
+          default: () => import('../page/buyer/dashbord/index.vue')
         }
       },
       {
         path: 'sale-record',
-        components:{
+        components: {
           helper: () => import('../page/buyer/sale-record/index.vue'),
-          default:() => import('../page/buyer/sale-record/index.vue'),
+          default: () => import('../page/buyer/sale-record/index.vue')
         }
       },
 
       {
         path: 'recharge',
-        components:{
+        components: {
           helper: () => import('../page/buyer/recharge/index.vue'),
-          default:() => import('../page/buyer/recharge/index.vue'),
+          default: () => import('../page/buyer/recharge/index.vue')
         }
       },
 
       {
         path: 'recharge-log',
-        components:{
+        components: {
           helper: () => import('../page/buyer/recharge-log/index.vue'),
-          default:() => import('../page/buyer/recharge-log/index.vue'),
+          default: () => import('../page/buyer/recharge-log/index.vue')
         }
       },
 
       {
         path: 'manual-lease',
-        components:{
+        components: {
           helper: () => import('../page/buyer/manual-lease/index.vue'),
-          default:() => import('../page/buyer/manual-lease/index.vue'),
+          default: () => import('../page/buyer/manual-lease/index.vue')
         }
       },
 
       {
         path: 'api-key',
-        components:{
+        components: {
           helper: () => import('../page/buyer/api-key/index.vue'),
-          default:() => import('../page/buyer/api-key/index.vue'),
+          default: () => import('../page/buyer/api-key/index.vue')
         }
       },
 
       {
         path: 'help',
-        components:{
+        components: {
           helper: () => import('../page/buyer/help/index.vue'),
-          default:() => import('../page/buyer/help/index.vue'),
+          default: () => import('../page/buyer/help/index.vue')
         }
-      },
-
-
-
+      }
     ]
-  },
+  }
 ]
 
 const router = createRouter({
@@ -80,13 +93,12 @@ const router = createRouter({
   routes: routes
 })
 
-
-  //路由守卫
+//路由守卫
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('stor')
-  next();
+  next()
   // const role = localStorage.getItem('ms_username');
   // NProgress.start(); //进度条
-});
+})
 
-export default router;
+export default router
